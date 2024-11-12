c814ecfe9d51ec8d88cd01b9a6b36376e6183845
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-11-12 23:15:19 +0100

    cors

--- SmilePro/settings.py
+++ SmilePro/settings.py
@@ -23,6 +23,7 @@ SHARED_APPS = [
     'drf_spectacular',
     'institution',
     'custom_user',
+    'corsheaders',
 ]
 
 TENANT_APPS = [
@@ -45,6 +46,7 @@ INSTALLED_APPS = SHARED_APPS + [
 
 MIDDLEWARE = [
     'django_tenants.middleware.main.TenantMainMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'SmilePro.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {

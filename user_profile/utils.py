f6a9c73b3db926111d581967ff105dab5fea6ed0
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-11-12 21:39:43 +0100

    minor

--- user_profile/utils.py
+++ user_profile/utils.py
@@ -46,6 +46,7 @@ def mark_occupied_slots(slots, appointments, other_appointments, office):
 
         for appointment in appointments:
             if appointment.start_time < slot_end_time and appointment.end_time > slot_start_time:
+                occupied_by_doctor = appointment.doctor
                 slot['status'] = 'zajęty'
                 slot['occupied_by'] = f"{occupied_by_doctor.name} {occupied_by_doctor.surname}"
                 slot_occupied = True
